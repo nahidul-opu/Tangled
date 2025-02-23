3207d32a44cc14524d44d1600a5aeec0c71b2968
Md. Nahidul Islam Opu <opu.nahidul@gmail.com>
2025-02-22 18:25:59 -0600

    Continue labelling

--- web-app/app.py
+++ web-app/app.py
@@ -38,7 +38,7 @@ def tangledReviewer():
 
 def remove_completed(files):
     if True:
-        completed = ["spring-boot", "guava", "pmd", "hbase", "ant"]
+        completed = ["spring-boot", "guava", "pmd", "hbase", "ant", "lucene-solr", "jna", "spring-framework", "sonarqube"]
         return [item for item in files if not any(sub in item for sub in completed)]
     else:
         return files
